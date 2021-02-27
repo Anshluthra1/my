810aea85b7177e46a26141356fadee10cda6e9a2
ANSH LUTHRA <76260663+Anshluthra1@users.noreply.github.com>
2021-02-27 22:50:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+consta hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+consta mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+consta menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+consta header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
